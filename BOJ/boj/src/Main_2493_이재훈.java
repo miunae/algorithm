dfd0eb19e85a4221b89509463d8792b62b63a6f5
miunae <miunae@naver.com>
2022-08-12 01:46:53 +0900

    0812 update

--- BOJ/boj/src/Main_2493_이재훈.java
+++ BOJ/boj/src/Main_2493_이재훈.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main_2493_이재훈 {
@@ -20,38 +17,20 @@ public class Main_2493_이재훈 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		Queue<Tower> queue = new LinkedList<>();
-		ArrayList<Integer> answer = new ArrayList<>();
+		Stack<Tower> stack = new Stack<>();
+		int height, index;
 		for(int i=1;i<=N;i++) {
-			Tower top = new Tower(i,Integer.parseInt(st.nextToken()));
-			queue.add(top);
-		}
-		Tower maxTower = new Tower(0,0);
-		Tower curMaxTower = new Tower(0,0);
-		while(!queue.isEmpty()) {
-			Tower tmp = queue.poll();
-			if(maxTower.height < tmp.height) {
-				maxTower.index = tmp.index;
-				maxTower.height = tmp.height;
-				curMaxTower.index = tmp.index;
-				curMaxTower.height = tmp.height;
-				answer.add(0);
+			height = Integer.parseInt(st.nextToken());
+			while(!stack.isEmpty()) {
+				if(stack.peek().height > height) {
+					System.out.print(stack.peek().index+" ");
+					break;
+				}
+				stack.pop();
 			}
-			if(tmp.height < curMaxTower.height) {
-				answer.add(curMaxTower.index);
-			}
-			if(tmp.height > curMaxTower.height) {
-				answer.add(maxTower.index);
-				curMaxTower.index = tmp.index;
-				curMaxTower.height = tmp.height;
-			}
-			if(queue.size() != 0 && tmp.height>queue.peek().height) {
-				curMaxTower.index = tmp.index;
-				curMaxTower.height = tmp.height;
-			}
-		}
-		for(int i=0;i<N;i++) {
-			System.out.print(answer.get(i)+" ");
+			if(stack.isEmpty())
+				System.out.print(0+" ");
+			stack.push(new Tower(i,height));
 		}
 		
 		
