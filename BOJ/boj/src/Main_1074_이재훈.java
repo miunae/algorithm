a2be7cb06b82eba33bf4ab0b6187f7eaa94a59c4
miunae <miunae@naver.com>
2022-08-17 21:22:14 +0900

    0817U

--- BOJ/boj/src/Main_1074_이재훈.java
+++ BOJ/boj/src/Main_1074_이재훈.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class MaIN_1074_이재훈 {
+public class Main_1074_이재훈 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
