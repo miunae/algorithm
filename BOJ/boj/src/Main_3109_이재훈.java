8ba6deea67616dd4d8321cc6aff0ac00cb955896
miunae <miunae@naver.com>
2022-08-19 01:50:25 +0900

    0819U

---
+++ BOJ/boj/src/Main_3109_이재훈.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_3109_이재훈 {
+	static int R,C;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+	}
+
+}
