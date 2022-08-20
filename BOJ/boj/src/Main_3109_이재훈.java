9eb36c54937a5bd3829c10c6e669154e3373c616
miunae <miunae@naver.com>
2022-08-21 02:16:13 +0900

    0821 update

--- BOJ/boj/src/Main_3109_이재훈.java
+++ BOJ/boj/src/Main_3109_이재훈.java
@@ -1,6 +1,8 @@
+import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main_3109_이재훈 {
@@ -10,6 +12,7 @@ public class Main_3109_이재훈 {
 	static int cnt=0;
 	static int[] dx = {-1,0,1};
 	static int[] dy = {1,1,1};
+	static ArrayList<Point> list;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,28 +23,36 @@ public class Main_3109_이재훈 {
 		for(int i=0;i<R;i++) {
 			arr[i] = br.readLine().toCharArray();
 		}
+		list = new ArrayList<>();
 		visited = new boolean[R][C];
 		for(int i=0;i<R;i++) {
 			dfs(i,0);
 		}
 		System.out.println(cnt);
 	}
-	static void dfs(int x, int y) {
+	static boolean dfs(int x, int y) {
 		if(y == C-1) {
 			cnt+=1;
-			return;
+			return true;
 		}
-		if(visited[x][y]) {
-			return;
-		}
+		
 		for(int d=0;d<3;d++) {
 			int nx = x+dx[d];
 			int ny = y+dy[d];
-			if(nx>=0 && nx < R && ny>=0 && ny < C && arr[nx][ny] != 'X') {
-				visited[nx][ny] = true;
-				dfs(nx,ny);
-				visited[nx][ny] = false;
+			if(nx>=0 && nx < R && ny>=0 && ny < C && arr[nx][ny] == '.') {
+				arr[nx][ny] = '-';
+				if(dfs(nx,ny)) return true;		
 			}
+		}
+		return false;
+	}
+	
+	static class Point {
+		int x;
+		int y;
+		Point(int x,int y){
+			this.x=x;
+			this.y=y;
 		}
 	}
 }
