250ccbe3809aa8adc43bd6021a5b9d17ccbf4354
miunae <miunae@naver.com>
2022-08-31 00:15:36 +0900

    0831

--- BOJ/boj/src/Main_1149_이재훈.java
+++ BOJ/boj/src/Main_1149_이재훈.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main_1149_이재훈 {
@@ -17,15 +16,11 @@ public class Main_1149_이재훈 {
 				cost[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-//		for (int i = 0; i < N; i++) {
-//			System.out.println(Arrays.toString(cost[i]));
-//		}
-		for (int i = 1; i < N; i++) {
+		for (int i=1;i<N;i++) {
 			cost[i][0] += Math.min(cost[i - 1][1], cost[i - 1][2]);
 			cost[i][1] += Math.min(cost[i - 1][0], cost[i - 1][2]);
-			cost[i][2] += Math.min(cost[i = 1][0], cost[i - 1][1]);
+			cost[i][2] += Math.min(cost[i - 1][0], cost[i - 1][1]);
 		}
-		System.out.println("hello");
-//		System.out.println(Math.min(cost[N = 1][0], Math.min(cost[N - 1][1], cost[N - 1][2])));
+		System.out.println(Math.min(cost[N - 1][0], Math.min(cost[N - 1][1], cost[N - 1][2])));
 	}
 }
