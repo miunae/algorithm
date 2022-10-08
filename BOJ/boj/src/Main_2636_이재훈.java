9c8bab88b4d7a921c885ca1fc14d952d65716d6b
miunae <miunae@naver.com>
2022-10-08 17:18:03 +0900

    1008

--- BOJ/boj/src/Main_2636_이재훈.java
+++ BOJ/boj/src/Main_2636_이재훈.java
@@ -1,17 +1,98 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main_2636_이재훈 {
 	static int N,M;
 	static int[][] map;
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0;j<M;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		boolean allMelt = false;
+		int time = 0;
+		int cnt = 0;
+		while(!allMelt) {
+			bfs(0,0);
+			cnt = melt(--time);
+			allMelt = true;
+			for(int i=0;i<N;i++) {
+				for(int j=0;j<M;j++) {
+					if(map[i][j] == 1) {
+						allMelt = false;
+					}
+				}
+			}
+		}
+		System.out.println(Math.abs(time));
+		System.out.println(cnt);
 	}
-
+	
+	static class Point{
+		int x,y;
+		public Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static void bfs(int x,int y) {
+		ArrayDeque<Point> deq = new ArrayDeque<>();
+		deq.add(new Point(x,y));
+		boolean[][] visited = new boolean[N][M];
+		visited[x][y] = true;
+		while(!deq.isEmpty()) {
+			Point cur = deq.poll();
+			int cx = cur.x;
+			int cy = cur.y;
+			for(int d=0;d<4;d++) {
+				int nx = cx + dx[d];
+				int ny = cy + dy[d];
+				if(nx>=0 && nx<N && ny>=0 && ny<M && !visited[nx][ny] && map[nx][ny] != 1) {
+					visited[nx][ny] = true;
+					deq.add(new Point(nx,ny));
+					map[nx][ny] = 2;
+				}
+			}
+		}
+	}
+	
+	static int melt(int time) {
+		ArrayDeque<Point> deq = new ArrayDeque<>();
+		deq.add(new Point(0,0));
+		boolean[][] visited = new boolean[N][M];
+		visited[0][0] = true;
+		int cnt = 0;
+		while(!deq.isEmpty()) {
+			Point cur = deq.poll();
+			int cx = cur.x;
+			int cy = cur.y;
+			for(int d=0;d<4;d++) {
+				int nx = cx + dx[d];
+				int ny = cy + dy[d];
+				if(nx>=0 && nx<N && ny>=0 && ny<M && !visited[nx][ny]) {
+					if(map[nx][ny] == 2) {
+						deq.add(new Point(nx,ny));
+						visited[nx][ny] = true;
+					}else if(map[nx][ny] == 1) {
+						map[nx][ny] = time;
+						cnt += 1;
+					}
+				}
+			}
+		}
+		return cnt;
+	}
 }
--- BOJ/boj/src/Main_9205_이재훈.java
+++ BOJ/boj/src/Main_9205_이재훈.java
@@ -1,16 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main_9205_이재훈 {
-
+	static String answer;
+	static Pos[] location;
+	static int n;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
 		for(int T=0;T<t;T++) {
-			int n = Integer.parseInt(br.readLine());
-			
+			answer = "sad";
+			n = Integer.parseInt(br.readLine());
+			location = new Pos[n+2];
+			for(int i=0;i<n+2;i++) {
+				st = new StringTokenizer(br.readLine());
+				location[i] = new Pos(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+			}
+			bfs();
+			System.out.println(answer);
 		}
 	}
-
+	
+	static class Pos{
+		int x,y;
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	static void bfs() {
+		ArrayDeque<Pos> deq = new ArrayDeque<>();
+		deq.add(location[0]);
+		boolean[] visited = new boolean[location.length+2];
+		visited[0] = true;
+		while(!deq.isEmpty()) {
+			Pos cur = deq.poll();
+			if(cur.x == location[n+1].x && cur.y == location[n+1].y) {
+				answer = "happy";
+				break;
+			}
+			for(int i=1;i<n+2;i++) {
+				if(!visited[i] && dist(cur,location[i])<=1000) {
+					deq.add(location[i]);
+					visited[i] = true;
+				}
+			}
+		}
+	}
+	
+	static int dist(Pos a,Pos b) {
+		return Math.abs(a.x-b.x)+Math.abs(a.y-b.y);
+	}
 }
